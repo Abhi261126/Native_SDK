6bc1571311f9221212b70f43eae83d9246f5f83f
Abhishek Singh <Abhishek_110388@yahoo.com>
2021-09-02 11:53:09 +0530

    Initial commit

---
+++ Example/Native_SDK/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Native_SDK
+//
+//  Created by 78590831 on 09/02/2021.
+//  Copyright (c) 2021 78590831. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
